3fcf580c25f800b833a046cfafde3ad4580a1c18
Fabricio Fernandes <fabricio_lkp@hotmail.com>
2021-07-12 21:02:25 -0300

    Alterações de Migration parte de tabela de Produto, Nova tabela pra compra e relacionamentos.

---
+++ Entities/Entities/Entities/Enums/EstadoCompra.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Entities.Enums
+{
+    public enum EstadoCompra
+    {
+        Produto_Carrinho = 1,
+        Produto_Comprado = 2,
+        Produto_Em_Preparacao = 3,
+        Produto_Caminho = 4,
+        Produto_Entregue = 5,
+        Produto_Recebido = 6,
+        Produto_Cancelado = 7
+    }
+}
--- Entities/Entities/Entities/Produto.cs
+++ Entities/Entities/Entities/Produto.cs
@@ -17,15 +17,44 @@ namespace Entities.Entities
         public int Id { get; set; }
 
         [Column("PRD_NOME")]
+        [MaxLength(255)]
         [Display(Name = "Nome")]
         public string Nome { get; set; }
+
+        [Column("PRD_DESCRICAO")]
+        [MaxLength(150)]
+        [Display(Name = "Descrição")]
+        public string Descricao { get; set; }
+
+        [Column("PRD_OBSERVACAO")]
+        [MaxLength(20000)]
+        [Display(Name = "Observação")]
+        public string Observacao { get; set; }
 
         [Column("PRD_VAL")]
         [Display(Name = "Valor")]
         public decimal Valor { get; set; }
 
+        [Column("PRD_QTD_ESTOQUE")]
+        [Display(Name = "Quantidade de Estoque")]
+        public int QuantidadeEstoque { get; set; }
+
+        [Display(Name = "Usuário")]
+        [ForeignKey("ApplicationUser")]
+        [Column(Order = 1)]
+        public string UserId { get; set; }
+        public virtual ApplicationUser ApplicationUser { get; set; }
+
         [Column("PRD_ESTD")]
         [Display(Name = "Estado")]
         public bool Estado { get; set; }
+
+        [Column("PRD_DATA_CADASTRO")]
+        [Display(Name = "Data de Cadastro")]
+        public DateTime DataCadastro { get; set; }
+
+        [Column("PRD_DATA_ALTERACAO")]
+        [Display(Name = "Data de Alteração")]
+        public DateTime DataAlteracao { get; set; }
     }
 }
